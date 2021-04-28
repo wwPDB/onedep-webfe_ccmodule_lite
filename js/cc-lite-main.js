ba7f40528dafe65a991fcbe35462e211d54e2205
Weslley Morellato <wbueno@ebi.ac.uk>
2021-04-28 07:52:58 -0400

    Updated messages in ligand page.

--- js/cc-lite-main.js
+++ js/cc-lite-main.js
@@ -51,6 +51,7 @@ var ChemCompLiteMod = {
 		GET_LIGAND_SUMMARY: CC_LITE_SESSION_DATA.servicePathPrefix+'/service/cc_lite/report/summary',
 		GET_REPORT_FILE: CC_LITE_SESSION_DATA.servicePathPrefix+'/service/cc_lite/report/file',
 		GET_REPORT_STATUS: CC_LITE_SESSION_DATA.servicePathPrefix+'/service/cc_lite/report/status',
+		RUN_LIGAND_ANALYSIS: CC_LITE_SESSION_DATA.servicePathPrefix+'/service/cc_lite/report/create',
 		CHECK_FOR_SMMRY_DATA: CC_LITE_SESSION_DATA.servicePathPrefix+'/service/cc_lite/view/ligandsummary/data_check',
 		LOAD_LIG_SMMRY_DATA: CC_LITE_SESSION_DATA.servicePathPrefix+'/service/cc_lite/view/ligandsummary/data_load',
 		LOAD_INSTNC_BRWSR: CC_LITE_SESSION_DATA.servicePathPrefix+'/service/cc_lite/view/instancebrowser',
